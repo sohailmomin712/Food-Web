2726f1ab06d7abc9ae8fe7e92f586a94782edada
Sohail Momin <zelan9691@gmail.com>
2025-01-22 12:11:13 +0530

    adding footer

--- Food-Website/src/components/Footer/Footer.jsx
+++ Food-Website/src/components/Footer/Footer.jsx
@@ -1,14 +1,44 @@
-import './Footer.css'
+import { assets } from "../../assets/assets";
+import "./Footer.css";
 const Footer = () => {
-    return (
-        <div className='footer' id='footer'>
-            <div className='footer-content'>
-                <div className='footer-content-left'></div>
-                <div className='footer-content-center'></div>
-                <div className='footer-content-right'></div>
-            </div>
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Savor the finest flavors with our freshly prepared, high-quality
+            meals, crafted with love and delivered right to your doorstep.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
-    )
-}
 
-export default Footer
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr></hr>
+      <p className="footer-copyright">Copyright 2024 © Tomato.com -All Right Reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
